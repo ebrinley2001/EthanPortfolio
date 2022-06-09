a654510f29564aa392b8a5f4e7992b773d318187
Ethan <66755110+ebrinley2001@users.noreply.github.com>
2022-06-09 10:51:56 -0400

    Changed Job Description

--- src/components/ResumePage/Jobs/PartnerServices.js
+++ src/components/ResumePage/Jobs/PartnerServices.js
@@ -3,7 +3,7 @@ function PartnerServices() {
     return (
         <>
             <div className="ResumeTextProps">
-                <span className="AltColor">public Experience </span><span>Partner Services (Asset Management) Intern </span><span className="AltColor">= </span><span>{String.fromCharCode(123)}</span>
+                <span className="AltColor">public Experience </span><span>Development / Asset Management Intern </span><span className="AltColor">= </span><span>{String.fromCharCode(123)}</span>
             </div>
             <div className="ResumeTextProps">
                 <span>Location </span><span className="AltColor">= </span><span>"Union Home Mortgage",</span>
@@ -14,11 +14,14 @@ function PartnerServices() {
             <div className="ResumeTextProps">
                 <span>Responsibilities </span><span className="AltColor">= </span>
                 <ul className="VerticalList">
-                    <li className="VerticalListItem">Internal Access Control,</li>
+                    <li className="VerticalListItem">[ Internal Access Control,</li>
                     <li className="VerticalListItem">Process Automation,</li>
                     <li className="VerticalListItem">Equipment Provisioning and Retreival,</li>
+                    <li className="VerticalListItem">Full Stack Application Development,</li>
+                    <li className="VerticalListItem">Application Implementation,</li>
+                    <li className="VerticalListItem">Package Management,</li>
                     <li className="VerticalListItem">Internal Application Creation,</li>
-                    <li className="VerticalListItem">Collaborative Project Work]”</li>
+                    <li className="VerticalListItem">Collaborative Project Work ]”</li>
                 </ul>
             </div>
             <span className="ResumeTextBracketClose">{String.fromCharCode(125)};</span>
